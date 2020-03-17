71f2d9b96e70fed03f564b29202a0bead0906026
ethanmaniss <54688877+ethanmaniss@users.noreply.github.com>
2020-03-16 19:39:59 -0700

    Project Commit
    commits

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+// https://answers.unity.com/questions/1002819/how-do-i-save-highscore.html
+
+public class ScoreController : MonoBehaviour
+{
+    public int score = 0;
+
+    public int highScore = 0;
+
+    void Start()
+    {
+
+        score = 0;
+
+        highScore = PlayerPrefs.GetInt("Highscore: ", highScore);
+    }
+
+    void Update()
+    {
+        if(score > highScore)
+        {
+            highScore = score;
+
+            PlayerPrefs.SetInt("Highscore: ", highScore);
+        }
+
+    }
+
+    void OnDestroy()
+    {
+        PlayerPrefs.SetInt("Highscore: ", highScore);
+        PlayerPrefs.Save();
+    }
+
+
+    public void UpdateScore()
+    {
+        GetComponent<Text>().text = "Score: " + score;
+        GetComponent<Text>().text = "Highscore: " + highScore;
+    }
+    
+}
